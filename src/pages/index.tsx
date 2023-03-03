53ec08428476b0ef59f2d6c58570a5209c329751
Yash Sahu <itsyashsahu@gmail.com>
2023-03-03 17:59:33 +0530

    trying to fix the env problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header'
 const inter = Inter({ subsets: ['latin'] })
-require("dotenv").config
 
 export default function Home() {
   return (
