f83e880a8508b84cb7100f8d38d2771707c780a7
Yash Sahu <itsyashsahu@gmail.com>
2023-03-03 16:15:55 +0530

    Fixed the redirecting button

--- src/pages/resultpage.tsx
+++ src/pages/resultpage.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 import dataJson from "../data.json"
@@ -33,9 +34,9 @@ const ResultPage = () => {
                         <h1 className="max-w-2xl mb-6 text-3xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">{signData?.title ? signData.title : "Some Error Occured"}</h1>
                         <p className="max-w-2xl mb-6 text-white lg:mb-8 md:text-lg lg:text-xl">{signData?.description ? signData.description : "Some Error Occured"} </p>
                         {/* Upload Next Image Button */}
-                        <a href="#" className="inline-flex items-center justify-center px-3 py-2 md:px-5 md:py-3 bg-white text-base font-medium text-center text-gray-900 border border-gray-300 rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+                        <Link href="/tryitnow" className="inline-flex items-center justify-center px-3 py-2 md:px-5 md:py-3 bg-white text-base font-medium text-center text-gray-900 border border-gray-300 rounded-xl hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
                             Upload Next Image
-                        </a>
+                        </Link>
                     </div>
                     {/* Image Corresponding to the traffic sign */}
                     <div className="drop-shadow-2xl lg:mt-0 lg:col-span-5 lg:flex ">
